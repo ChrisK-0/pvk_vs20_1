b5b7a401c4c0f11ba12915dba7b6217e7510c2b4
Chris Kottisse <chris.kottisse@khk.ee>
2021-01-12 11:30:43 +0200

    määratud ja võetud kasutusele controlleri meetod ning võimalikud parameetrid

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -11,4 +11,11 @@ class Pages
     {
         echo 'Pages class is loaded<br>';
     }
+
+    public function index(){
+        echo 'index method is loaded<br>';
+    }
+    public function about(){
+        echo 'about method is loaded<br>';
+    }
 }
