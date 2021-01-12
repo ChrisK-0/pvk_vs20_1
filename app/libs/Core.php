a6b97fc0041a7d8cf77aadc36d8b6ce25ad1fed7
Chris Kottisse <chris.kottisse@khk.ee>
2021-01-12 11:08:54 +0200

    controller määratud ja võetud kasutusele controlleri-objekti näol

---
+++ app/libs/Core.php
@@ -0,0 +1,39 @@
+<?php
+
+
+class Core
+{
+    protected $currentController = 'Pages';
+    protected $currentMethod = 'Index';
+    protected $params = [];
+    /**
+     * Core constructor.
+     */
+    public function __construct(){
+        $url = $this->getUrl();
+        $controllerName = ucwords($url[0]);
+        $controllerFile = '../apps/controllers/'.$controllerName.'.php';
+
+        if(file_exists($controllerFile)){
+            $this->currentController = $controllerName;
+            unset($url[0]);
+        }
+    require_once '../app/controllers/'.$this->currentController.'.php';
+    $this->currentController = new $this->currentController;
+        echo $this->currentController;
+        print_r($url);
+    }
+    // get url data
+    public function getUrl(){
+        if(isset($_GET['url'])){
+            $url = $_GET['url'];
+            $url = rtrim($url, '/');
+            $url = htmlentities($url);
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            //echo $url;
+            print_r($url);
+            return $url;
+        }
+    }
+}
