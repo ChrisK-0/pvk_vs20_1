bf1bccba05e162def9c330cc79c45be7a02dbc28
Chris Kottisse <chris.kottisse@khk.ee>
2021-01-18 13:37:37 +0200

    Rakenduse põhi/raamistik on valmis

--- app/config/constants.php
+++ app/config/constants.php
@@ -1,7 +1,14 @@
 <?php
-// projekt name
-define('PROJECT_NAME', 'pvk_vs20_1');
-//app root dir
+// Project name
+define('PROJECT_NAME', 'projectname');
+// App root dir
 define('APPROOT', dirname(dirname(__FILE__)));
-// url root
+// URL root
 define('URLROOT', 'http://'.$_SERVER['HTTP_HOST'].'/'.PROJECT_NAME);
+
+// db params
+define('DB_HOST', 'host');
+define('DB_USER', 'user');
+define('DB_PASS', 'pass');
+define('DB_NAME', 'dbname');
+
--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -3,24 +3,21 @@
 
 class Pages extends Controller
 {
+
     /**
      * Pages constructor.
      */
     public function __construct()
     {
-        //echo 'Pages class is loaded<br>';
+        $this->pagesModel = $this->model('Page');
     }
+
     public function index(){
-        //echo 'index method is loaded<br>';
+        // echo 'index method is loaded<br>';
         $data = array(
             'title' => 'Welcome to Pages',
-            'content' => 'Pages index view is loaded'
-            );
+            'content' => 'Pages index view is loaded by MVC'
+        );
         $this->view('pages/index', $data);
-    }
-    public function about(){
-        //echo 'about method is loaded<br>';
-        $data = array('content' => 'Pages about view is loaded by MVC');
-        $this->view('pages/about', $data);
     }
 }
